cc03c8df147a8ef62b4dc6fa0afe992e1308c203
kingno3l <nuelluiz@gmail.com>
2024-02-23 14:16:57 +0100

    changes done

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_02_114442) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_03_182806) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "greetings", force: :cascade do |t|
-    t.string "message"
+  create_table "messages", force: :cascade do |t|
+    t.string "greeting"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
